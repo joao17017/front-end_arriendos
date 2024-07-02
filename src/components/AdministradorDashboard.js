c987f47b1ea17d328c4813dcd2a4f91c48de35f1
Berna2002 <153837347+Berna2002@users.noreply.github.com>
2024-07-01 20:32:58 -0500

    Update AdministradorDashboard.js

--- src/components/AdministradorDashboard.js
+++ src/components/AdministradorDashboard.js
@@ -1,11 +1,45 @@
-// AdministradorDashboard.js
-import React from 'react';
+import React, { useState } from 'react';
+import NavBarAdministrador from './NavBarAdministrador';
+import './AdministradorDashboard.css';
 
 const AdministradorDashboard = () => {
+  const [showUserManagement, setShowUserManagement] = useState(false);
+  const [activeUserType, setActiveUserType] = useState('');
+
+  const handleUserManagementClick = () => {
+    setShowUserManagement(!showUserManagement);
+  };
+
+  const handleSelectUserType = (type) => {
+    setActiveUserType(type);
+  };
+
   return (
     <div>
-      <h1>Bienvenido Administrador</h1>
-      <p>Este es tu panel de control.</p>
+      <NavBarAdministrador onGestionarUsuariosClick={handleUserManagementClick} />
+      <div className="dashboard-container">
+        {!showUserManagement ? (
+          <div className="welcome-message">
+            <h1>Bienvenido al Panel de Administrador</h1>
+          </div>
+        ) : (
+          <div className="user-management-container">
+            <div className="sidebar">
+              <button onClick={() => handleSelectUserType('Estudiante')}>Estudiante</button>
+              <button onClick={() => handleSelectUserType('Arrendador')}>Arrendador</button>
+              <button onClick={() => handleSelectUserType('Administrador')}>Administrador</button>
+            </div>
+            <div className="main-content">
+              {activeUserType && (
+                <>
+                  <h2>Gestionar Usuario: {activeUserType}</h2>
+                  <input type="text" placeholder={`Buscar ${activeUserType} por nombre o ID`} />
+                </>
+              )}
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
