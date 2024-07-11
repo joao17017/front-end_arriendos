b9860888745114114250cde7b878b04163dd2c07
KevinKamh <153756479+KevinKamh@users.noreply.github.com>
2024-07-10 23:51:45 -0500

    Diseño Anuncios por activar Arrendador

--- src/components/AnunciosPorActivar.js
+++ src/components/AnunciosPorActivar.js
@@ -1,10 +1,12 @@
-// src/components/AnunciosPorActivar.js
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import NavBarArrendador from './NavBarArrendador';
 import { jwtDecode } from 'jwt-decode';
-import './EstudianteDashboard.css';
+import './MisDepartamentos.css';
+import './AnunciosPorActivar.css';
+import { FaInfoCircle } from 'react-icons/fa';
+
 
 const AnunciosPorActivar = () => {
   const [solicitudes, setSolicitudes] = useState([]);
@@ -25,7 +27,7 @@ const AnunciosPorActivar = () => {
 
         const response = await axios.get(`http://localhost:3000/solicitudes-activacion/arrendador/${id_arrendador}`, {
           headers: {
-            Authorization: `Bearer ${token}`, // Corrección del encabezado de autorización
+            Authorization: `Bearer ${token}`,
           },
         });
 
@@ -42,24 +44,29 @@ const AnunciosPorActivar = () => {
   return (
     <div>
       <NavBarArrendador />
-      <div className="dashboard">
-        <h1>Anuncios por Activar de Arrendador</h1>
-        <p>Bienvenido a tu listado de departamentos por activarse.</p>
-        <div className="cards-container">
+      <div className="container mt-4">
+        <h1 className="mb-4">Anuncios por Activar de Arrendador</h1>
+        <div className="row mx-1 portfolio-container">
           {solicitudes.map(solicitud => (
-            <div className="card" key={solicitud.id_solicitud}>
-              {solicitud.Departamento ? (
-                <>
-                  <h3>{solicitud.Departamento.nombre}</h3>
-                  <p>{solicitud.Departamento.descripcion}</p>
-                  <p><strong>Estado:</strong> {solicitud.estado}</p>
-                  {solicitud.Departamento.imagen && (
-                    <img src={`http://localhost:3000/${solicitud.Departamento.imagen}`} alt={solicitud.Departamento.nombre} className="card-image" />
-                  )}
-                </>
-              ) : (
-                <p>Departamento no encontrado</p>
-              )}
+            <div className="col-lg-4 col-md-6 col-sm-12 p-0 portfolio-item" key={solicitud.id_solicitud}>
+              <div className="position-relative overflow-hidden">
+                <div className="portfolio-img d-flex align-items-center justify-content-center">
+                  <img
+                    src={solicitud.Departamento && solicitud.Departamento.imagen ? `http://localhost:3000/${solicitud.Departamento.imagen}` : "/images/default-image.png"}
+                    alt={solicitud.Departamento ? solicitud.Departamento.nombre : 'Imagen no disponible'}
+                    className="img-fluid"
+                    style={{ width: '100%', height: '200px', objectFit: 'cover' }}
+                  />
+                </div>
+                <div className="portfolio-text bg-secondary d-flex flex-column align-items-center justify-content-center">
+                  <h4 className="text-white mb-4">{solicitud.Departamento ? solicitud.Departamento.nombre : 'Departamento no encontrado'}</h4>
+                  <div className="d-flex align-items-center justify-content-center">
+                    <span className="estado-texto">
+                      <FaInfoCircle className="estado-icono" /> {solicitud.estado}
+                    </span>
+                  </div>
+                </div>
+              </div>
             </div>
           ))}
         </div>
